98405fdebc47feefdca2b290995b6937adfd6603
Marcos O. Alves <eng.marcos.alves@gmail.com>
2018-09-02 12:11:21 -0300

    configuração do JPA e Hibernate para funcionar com MySQL 8.0.12

--- src/main/java/br/com/geekcode/curso/config/SpringJpaConfig.java
+++ src/main/java/br/com/geekcode/curso/config/SpringJpaConfig.java
@@ -3,7 +3,10 @@ package br.com.geekcode.curso.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaDialect;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
@@ -16,8 +19,8 @@ public class SpringJpaConfig {
     public DataSource dataSource(){
         DriverManagerDataSource ds = new DriverManagerDataSource();
         ds.setDriverClassName("com.mysql.jdbc.Driver");
-        ds.setUrl("jdbc:mysql://localhost:3306/test?createDatabaseIfNotExist=true");
-        ds.setUsername("Marcos");
+        ds.setUrl("jdbc:mysql://localhost:3306/dbcurso?createDatabaseIfNotExist=true&useTimezone=true&serverTimezone=UTC");
+        ds.setUsername("root");
         ds.setPassword("Marcos#123");
         return ds;
     }
@@ -27,17 +30,26 @@ public class SpringJpaConfig {
         LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
         factory.setDataSource(dataSource());
         factory.setPackagesToScan("br.com.geekcode.curso.domain");
+        factory.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
         factory.setJpaProperties(jpaProperties());
         factory.afterPropertiesSet();
         return factory.getObject();
     }
 
     @Bean
+    public JpaTransactionManager transactionManager() {
+        JpaTransactionManager tx = new JpaTransactionManager();
+        tx.setEntityManagerFactory(entityManagerFactory());
+        tx.setJpaDialect(new HibernateJpaDialect());
+        return tx;
+    }
+
     private Properties jpaProperties() {
         Properties properties = new Properties();
+        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL8Dialect");
         properties.setProperty("hibernate.show_sql", "true");
         properties.setProperty("hibernate.format_sql", "true");
-        properties.setProperty("hibernate.hbm2dll.auto", "update");
+        properties.setProperty("hibernate.hbm2ddl.auto", "update");
         return properties;
     }
 }
