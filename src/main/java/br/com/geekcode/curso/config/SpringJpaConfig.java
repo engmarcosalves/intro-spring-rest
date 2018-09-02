b90cd8b63b62dfe2e874e1f024bed63fc13357a3
Marcos O. Alves <eng.marcos.alves@gmail.com>
2018-09-02 10:31:58 -0300

    first commit

---
+++ src/main/java/br/com/geekcode/curso/config/SpringJpaConfig.java
@@ -0,0 +1,43 @@
+package br.com.geekcode.curso.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+public class SpringJpaConfig {
+
+    @Bean
+    public DataSource dataSource(){
+        DriverManagerDataSource ds = new DriverManagerDataSource();
+        ds.setDriverClassName("com.mysql.jdbc.Driver");
+        ds.setUrl("jdbc:mysql://localhost:3306/test?createDatabaseIfNotExist=true");
+        ds.setUsername("Marcos");
+        ds.setPassword("Marcos#123");
+        return ds;
+    }
+
+    @Bean
+    public EntityManagerFactory entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+        factory.setDataSource(dataSource());
+        factory.setPackagesToScan("br.com.geekcode.curso.domain");
+        factory.setJpaProperties(jpaProperties());
+        factory.afterPropertiesSet();
+        return factory.getObject();
+    }
+
+    @Bean
+    private Properties jpaProperties() {
+        Properties properties = new Properties();
+        properties.setProperty("hibernate.show_sql", "true");
+        properties.setProperty("hibernate.format_sql", "true");
+        properties.setProperty("hibernate.hbm2dll.auto", "update");
+        return properties;
+    }
+}
---
+++ src/main/java/br/com/geekcode/curso/config/SpringRootConfig.java
@@ -0,0 +1,13 @@
+package br.com.geekcode.curso.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@EnableTransactionManagement
+@ComponentScan("br.com.geekcode.curso")
+public class SpringRootConfig {
+}
